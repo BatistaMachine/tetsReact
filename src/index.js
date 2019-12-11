ffdcc630dd217752b5a0a06c2be95360ffff0184
BatistaMachine <33598815+BatistaMachine@users.noreply.github.com>
2019-12-11 14:03:51 +0100

    End add extrapoint

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import styles from "./styles.css";
+/*https://fr.reactjs.org/tutorial/tutorial.html#wrapping-up
+point 6 faire les autres points 1 à 5 */
 function Square(props) {
   return (
     <button className="square" onClick={props.onClick}>
@@ -50,12 +53,13 @@ class Game extends React.Component {
           squares: Array(9).fill(null)
         }
       ],
+      stepNumber: 0,
       xIsNext: true
     };
   }
 
   handleClick(i) {
-    const history = this.state.history;
+    const history = this.state.history.slice(0, this.state.stepNumber + 1);
     const current = history[history.length - 1];
     const squares = current.squares.slice();
     if (calculateWinner(squares) || squares[i]) {
@@ -68,22 +72,35 @@ class Game extends React.Component {
           squares: squares
         }
       ]),
+      stepNumber: history.length,
       xIsNext: !this.state.xIsNext
     });
   }
-
+  jumpTo(step) {
+    this.setState({ stepNumber: step, xIsNext: step % 2 === 0 });
+  }
   render() {
     const history = this.state.history;
-    const current = history[history.length - 1];
+    const current = history[this.state.stepNumber];
     const winner = calculateWinner(current.squares);
-
+    const moves = history.map((step, move) => {
+      const desc = move
+        ? "Revenir au tour n° " + move
+        : "Revenir au début de la partie";
+      return (
+        <li key={move}>
+          <button onClick={() => this.jumpTo(move)}>{desc}</button>
+        </li>
+      );
+    });
     let status;
     if (winner) {
       status = "Winner: " + winner;
+    } else if (this.state.history.length === 10) {
+      status = "Match nul";
     } else {
       status = "Next player: " + (this.state.xIsNext ? "X" : "O");
     }
-
     return (
       <div className="game">
         <div className="game-board">
@@ -91,7 +108,7 @@ class Game extends React.Component {
         </div>
         <div className="game-info">
           <div>{status}</div>
-          <ol>{/* TODO */}</ol>
+          <ol>{moves}</ol>
         </div>
       </div>
     );
